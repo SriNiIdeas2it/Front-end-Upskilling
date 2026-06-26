2bc05ab9c0034541ce482662cbd66a54208425c1
SriNiIdeas2it <srinivasan.amirthalingam@ideas2it.com>
2026-06-27 03:06:54 +0530

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,20 @@
+
+// function test(){
+//     for(var i= 1;i<=5;i++){
+//     setTimeout(() => {
+//         console.log(i);
+//     }, i*1000);
+//     }
+    
+//     console.log("test");
+// }
+function test(){
+    for(let i= 1;i<=5;i++){
+    setTimeout(() => {
+        console.log(i);
+    }, i*1000);
+    }  
+    console.log("test");
+}
+
+test();
